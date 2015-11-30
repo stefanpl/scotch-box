076894dfe682ba71f3b1d5cc1a26d42888ffd827
Stefan Pl <sploesser@vrm.de>
2015-11-30 17:12:14 +0100

    provisioning vim and zsh

---
+++ provision.sh
@@ -0,0 +1,17 @@
+if [ ! -d /home/vagrant/.oh-my-zsh ]; then
+	git clone https://github.com/stefanpl/oh-my-zsh .oh-my-zsh
+	ln -s /home/vagrant/.oh-my-zsh/.zshrc /home/vagrant/.zshrc
+	sudo apt-get install -y zsh
+	sudo usermod -s /bin/zsh vagrant
+	touch /home/vagrant/.bash_aliases_private
+fi
+
+if [ ! -d /home/vagrant/.vim ]; then
+	git clone https://github.com/stefanpl/vim-config .vim
+	cd /home/vagrant/.vim && git submodule update --init --recursive
+	ln -s /home/vagrant/.vim/.vimrc /home/vagrant/.vimrc
+fi
+
+sudo apt-get update
+sudo apt-get install -y php5-dev
+
